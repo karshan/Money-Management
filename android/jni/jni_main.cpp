d0894aa257891c23ddca3edcfe9adfcc6f233c86
Karshan Sharma <sharma34@illinois.edu>
2011-07-21 19:12:19 -0500

    proof of concept. part of moneycl ported to android due to thread issues its not stable

--- android/jni/jni_main.cpp
+++ android/jni/jni_main.cpp
@@ -79,19 +79,75 @@ int jscanf(JNIEnv *env, jobject thiz, const char *fmt, ...)
     return n;
 }
 
+void account_menu(JNIEnv *env, jobject thiz, account & acc)
+{
+    jprintf(env, thiz, "Account Menu isn't yet available\n");
+}
+
 void Java_com_blur_money_maingui_jni_1main(JNIEnv *env, jobject thiz)
 {
-    std::string fname("data.bin");
+    int chc;
+    std::string fname("/sdcard/data.bin");
     file_bank bank(fname);
 
-    int chc;
+    do {
+        jprintf(env, thiz, "Main Menu: (file: %s)\n", fname.c_str());
+        jprintf(env, thiz, "accounts:\n\tid\tname\n");
+        for (std::vector<account>::const_iterator it = bank.accounts_begin();
+             it != bank.accounts_end(); it++)
+            jprintf(env, thiz, "\t%u\t%s\n", (*it).get_id(), (*it).name.c_str());
 
-    while(1) {
+        jprintf(env, thiz, "0)exit\n"
+                           "1)load\n"
+                           "2)save\n"
+                           "3)add account\n"
+                           "4)edit account\n"
+                           "5)delete account\n"
+                           "enter choice: ");
+        chc = 0;
         jscanf(env, thiz, "%d", &chc);
-        if (jprintf(env, thiz, "recvd: %d\n", chc) < 0)
+
+        jprintf(env, thiz, "Yowza\n");
+
+        char acc[100];
+        unsigned int id;
+        switch(chc) {
+        case 0:
             break;
-        if (chc == 0)
+        case 1:
+            bank.load();
             break;
-    }
-    jprintf(env, thiz, "exiting\n");
+        case 2:
+            bank.save();
+            break;
+        case 3:
+            jprintf(env, thiz, "enter account name: ");
+            jscanf(env, thiz, "%s", acc);
+            jprintf(env, thiz, "Yowza\n");
+            bank.add_account(account(std::string(acc)));
+            break;
+        case 4:
+            jprintf(env, thiz, "enter account id: ");
+            id = 0;
+            jscanf(env, thiz, "%u", &id);
+            jprintf(env, thiz, "Yowza\n");
+            try {
+                account_menu(env, thiz, bank.get_account(id));
+            }
+            catch (bad_id & e) {
+                jprintf(env, thiz, "%s\n", e.what());
+            }
+            break;
+        case 5:
+            jprintf(env, thiz, "enter account id: ");
+            jscanf(env, thiz, "%u", &id);
+            jprintf(env, thiz, "Yowza\n");
+            if (!bank.delete_account(id))
+                jprintf(env, thiz, "Failed! bad id\n");
+            break;
+        default:
+            jprintf(env, thiz, "bad choice\n");
+        }
+    } while(chc != 0);
+    jprintf(env, thiz, "Goodbye\n");
 }
